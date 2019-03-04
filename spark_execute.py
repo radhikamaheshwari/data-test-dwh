3767355551e6411e07ffc27fb4bdf83d8ec3b949
radhikamaheshwari <38325302+radhikamaheshwari@users.noreply.github.com>
2019-03-04 22:39:41 +0800

    Update spark_execute.py

--- spark_execute.py
+++ spark_execute.py
@@ -3,7 +3,7 @@ import pyspark
 import os
 import visualize_dag
 from pyspark import SparkContext, SparkConf
-from pyspark.sql import SQLContext
+from pyspark.sql import SQLContext, HiveContext
 from pyspark.sql import HiveContext
 from pyspark.sql import functions as F
 from pyspark.sql.functions import udf, encode
@@ -15,7 +15,7 @@ conf = SparkConf() \
     .setAppName("Run SQl Parallelly as per DAG") \
 
 sc = SparkContext(conf=conf)
-sqlContext = SQLContext(sc)
+hiveContext = HiveContext(sc)
 
 spark_dag=[]
 for element in visualize_dag.stack:
@@ -27,29 +27,32 @@ for element in visualize_dag.stack:
 
 
 def read_data(path):
-    return sqlContext.read.csv(path)
+    #return sqlContext.read.csv(path)
+    return hiveContext.read.format("csv").option("header", "true").option("inferSchema", "true").load(path)
 
 
 def calling():
     # sqlContext.sql("create database if not exists raw")
     # sqlContext.sql("use raw")
-    categories= read_data(pwd+'/raw/inventory_items').write.saveAsTable("raw_categories",mode="overwrite")
+    categories= read_data(pwd+'/raw/categories').write.saveAsTable("raw_categories",mode="overwrite")
     inventory_items=read_data(pwd+'/raw/inventory_items').write.saveAsTable("raw_inventory_items", mode="overwrite")
     pictures = read_data(pwd+'/raw/pictures').write.saveAsTable("raw_pictures",mode="overwrite")
-    product_groups = read_data(pwd+'/raw/inventory_items').write.saveAsTable("raw_product_groups",mode="overwrite")
-    products = read_data(pwd+'/raw/inventory_items').write.saveAsTable("raw_products",mode="overwrite")
-    purchase_items = read_data(pwd+'/raw/inventory_items').write.saveAsTable("raw_purchase_items",mode="overwrite")
-    purchase_line_items = read_data(pwd+'/raw/inventory_items').write.saveAsTable("raw_purchase_line_items",mode="overwrite")
-    purchases = read_data(pwd+'/raw/inventory_items').write.saveAsTable("raw_purchases",mode="overwrite")
+    product_groups = read_data(pwd+'/raw/product_groups').write.saveAsTable("raw_product_groups",mode="overwrite")
+    products = read_data(pwd+'/raw/products').write.saveAsTable("raw_products",mode="overwrite")
+    purchase_items = read_data(pwd+'/raw/purchase_items').write.saveAsTable("raw_purchase_items",mode="overwrite")
+    purchase_line_items = read_data(pwd+'/raw/purchase_line_items').write.saveAsTable("raw_purchase_line_items",mode="overwrite")
+    purchases = read_data(pwd+'/raw/purchases').write.saveAsTable("raw_purchases",mode="overwrite")
+    variants = read_data(pwd + '/raw/variants').write.saveAsTable("raw_variants", mode="overwrite")
 
     # sqlContext.sql("create database if not exists tmp")
     # sqlContext.sql("use tmp")
     for element in spark_dag:
         element=element.replace('tmp_','tmp/')+'.sql'
+        element=element.replace('final_','final/')
         print (element)
         with open(os.getcwd() + '/'+element) as sqlfile:
             dfsql=sqlfile.read()
-            sqlContext.sql(dfsql);
+            hiveContext.sql(dfsql);
         #sqlContext.sql(os.getcwd() + '/' + element).toDF().saveAsTable(element, mode="overwrite")
 
 
